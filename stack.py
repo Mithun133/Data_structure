6f6b576b6f26321347540e103312e6106bc251d5
unknown <mithunchandradas133@gmail.com>
2021-07-02 18:10:09 +0100

    stack implementation done

---
+++ stack.py
@@ -0,0 +1,71 @@
+# Stack implementation in python
+
+
+# Creating a stack
+def create_stack():
+    stack = []
+    return stack
+
+
+# Creating an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+
+
+# Adding items into the stack
+def push(stack, item):
+    stack.append(item)
+    print("pushed item: " + item)
+
+
+# Removing an element from the stack
+def pop(stack):
+    if (check_empty(stack)):
+        return "stack is empty"
+
+    return stack.pop()
+
+
+stack = create_stack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+push(stack, str(4))
+print("popped item: " + pop(stack))
+print("stack after popping an element: " + str(stack))
+
+
+
+
+
+
+
+
+
+
+# Creating a class
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def add(self, dataval):
+# Use list append method to add element
+        if dataval not in self.stack:
+            self.stack.append(dataval)
+            return True
+        else:
+            return False
+# Use peek to look at the top of the stack
+
+    def peek(self):     
+	    return self.stack[-1]
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.peek()
+print(AStack.peek())
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
